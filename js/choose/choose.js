cb91ecc1b513e01cc6ac48a5553b8a049a1f478d
chsm1998 <1978364643@qq.com>
2018-04-23 19:46:12 +0800

    修复路径错误

--- js/choose/choose.js
+++ js/choose/choose.js
@@ -2,5 +2,5 @@ $('.sex').on('click', function () {
     var px = -117;
     $('.sex').css('background-image', '');
     $(this).css('background-position-x', px * $(this).index() + 'px')
-        .css('background-image', 'url(images/register/role_hover.jpg)');
+        .css('background-image', 'url(images/choose/role_hover.jpg)');
 })
