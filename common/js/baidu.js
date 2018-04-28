3f443d69dc06cb22ac1c067f2c4a9e5bab5d92cf
chsm1998 <1978364643@qq.com>
2018-04-28 09:58:35 +0800

    添加百度统计js

---
+++ common/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?f2f62567ad8e4a51daffab20e30dba05";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
