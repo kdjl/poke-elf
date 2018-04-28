9d1e58bbf67014f2e1d38cdc7e25c856408d4246
chsm1998 <1978364643@qq.com>
2018-04-28 10:09:33 +0800

    完善百度统计js

--- common/js/baidu.js
+++ common/js/baidu.js
@@ -1,7 +1,17 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?f2f62567ad8e4a51daffab20e30dba05";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+if (window.location.href.indexOf('kdjl') != -1) {
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?03f3ce22c46b79ed722e5554c00dae74";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+} else {
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?f2f62567ad8e4a51daffab20e30dba05";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
